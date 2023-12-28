cf16abfe49e086c1e75a0b2d56523f8f7aa65d73
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-12-28 17:42:32 +0900

    update contract

--- gaslessnfts/contracts/Contract.sol
+++ gaslessnfts/contracts/Contract.sol
@@ -1,19 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract BasedBicoNFT is ERC721 {
-    using Counters for Counters.Counter;
-
-    Counters.Counter private _tokenIdCounter;
-
     constructor() ERC721("Based Biconomy SDK NFT", "BBNFT") {}
 
+    uint256 public tokenId = 0;
+
     function safeMint(address to) public {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
         _safeMint(to, tokenId);
+        tokenId++;
     }
 }
